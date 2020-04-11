5728a6042cc98eb05f21b42705b5f03876d89009
AdwaitKamdi <62836411+AdwaitKamdi@users.noreply.github.com>
2020-04-11 20:45:20 +0530

    Add files via upload

---
+++ c20-master/sketch.js
@@ -0,0 +1,36 @@
+
+var r = 0;
+var g = 50;
+var b=255;
+var rectmoving(600,200,50,20);
+var ellipse;
+
+// INITIALIZE ALL YOUR VARIABLES
+// Ideally you would create 3 variables. red blue green or r, g, b
+
+
+function setup(){
+
+  createCanvas(1200,400);
+  CanvasColor="pink";
+}
+
+
+function draw(){
+  if (rectmoving.x=ellipse.x){
+    backgroundColor="cyan";
+  }
+  rectmoving.shapeColor="green";
+  rectmoving.x=mouse.x;
+  // change the value of Red based on the mouse movement about the X axis
+  // change the value of Green based on the mouse movement about the X axis
+  // change the value of Blue based on the mouse movement about the X axis
+
+  // Use the map() function to do so. 
+
+  // Pass the values to the background() function you have learnt previously.
+
+  // Create an ellipse that will move around with you mouse about the X axis.
+  // Remember to fill the canvas with white paint before creating the ellipse.
+  
+}
